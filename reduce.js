5ca641bd300fadcef8a45a581e67d33240ab41a0
Daniel Potts <djpotts2891@gmail.com>
2023-05-25 21:34:32 +0000

    lesson 2 complete

--- reduce.js
+++ reduce.js
@@ -30,10 +30,28 @@ const teamMembers = [
     name: 'Kelly',
     profession: 'Designer',
     yrsExperience: 3
+  },
+  {
+    name: 'Kelly',
+    profession: 'Designer2',
+    yrsExperience: 3
   }
 ];
 
 // Totaling a specific object property
 
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0)
+console.log(totalExperience)
 
 // Grouping by a property, and totaling it too
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession
+  if (!acc[key]){
+    acc[key] = curr.yrsExperience
+  } else {
+    acc[key] += curr.yrsExperience
+  }
+  return acc
+},{});
+
+console.log(experienceByProfession)
